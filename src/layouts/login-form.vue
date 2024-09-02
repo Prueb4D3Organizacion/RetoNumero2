c4c4b90daf7647059126ebfc5067e8d86f0b0795
PaoloBellezaTelloo <U20191A137@upc.edu.pe>
2024-09-02 18:10:12 -0500

    Reto2

---
+++ src/layouts/login-form.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+import RegisterForm from './register-form.vue'
+
+const username = ref('')
+const password = ref('')
+const userType = ref('')
+const butonLogin = ref('')
+const butonSwitchRegister = ref(false)
+</script>
+
+<template>
+  <div v-if="butonSwitchRegister">
+    <RegisterForm />
+  </div>
+  <div v-else>
+    <h2>Login</h2>
+    <label for = "username">Username: </label>
+    <input v-model="username" type="text" id="username"><br><br>
+    <label for = "password">Password: </label>
+    <input v-model="password" type = "password" id="password"><br><br>
+    <label>User Type: </label>
+    <input v-model = "userType" type = "radio" value = "Usuario" id="user">
+    <label for = "user">Usuario</label>
+    <input v-model="userType" type="radio" value="Administrador" id="admin">
+    <label for = "admin">Administrador</label><br><br>
+    <button @click="butonLogin">Login</button><br><br>
+    <button @click ="butonSwitchRegister = ! butonSwitchRegister">Switch to Register</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/register-form.vue
@@ -0,0 +1,40 @@
+<script setup>
+import LoginForm from './login-form.vue'
+import { ref } from 'vue'
+const username = ref('')
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+const userType = ref('')
+const butonRegister = ref('')
+const butonSwitchLogin = ref(false)
+</script>
+
+<template>
+  <div v-if="butonSwitchLogin">
+    <login-form/>
+  </div>
+  <div v-else>
+    <h2>Register</h2>
+    <label for="username">Username: </label>
+    <input v-model="username" type="text" id="username"><br><br>
+    <label for="email">Email: </label>
+    <input v-model="email" type="email" id="email"><br><br>
+    <label for="password">Password: </label>
+    <input v-model="password" type="password" id="password"><br><br>
+    <label for="confirmPassword">Confirm Password: </label>
+    <input v-model="confirmPassword" type="password" id="confirmPassword"><br><br>
+    <label>User Type: </label>
+    <input v-model="userType" type="radio" value="Usuario" id="user">
+    <label for="user">Usuario</label>
+    <input v-model="userType" type="radio" value="Administrador" id="admin">
+    <label for="admin">Administrador</label><br><br>
+    <button @click="butonRegister">Register</button><br><br>
+    <button @click="butonSwitchLogin = ! butonSwitchLogin">Switch to Login</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
